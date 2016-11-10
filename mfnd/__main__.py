3070ac8e337580958cc9db23f5539dc57636ec7d
mes32 <mes32@users.noreply.github.com>
2016-11-10 00:56:42 -0500

    misc: Create empty untracked directory data for project data

--- mfnd/__main__.py
+++ mfnd/__main__.py
@@ -4,10 +4,11 @@ MFND - A simple to-do list application
 """
 
 import datetime
+import sqlite3
 
 def main():
     today = datetime.date.today()
-    print( today.strftime("main MFND - %B %d, %Y") )
+    print( today.strftime("MFND - %B %d, %Y") )
 
 if  __name__ =='__main__':
     main()
