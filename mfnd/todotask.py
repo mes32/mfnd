8fe04b096348ab81d31f59030a22b943e548dc1f
mes32 <mes32@users.noreply.github.com>
2016-12-17 12:55:27 -0500

    refactor: Remove unneccessary variables from class 'TodoTask'

--- mfnd/todotask.py
+++ mfnd/todotask.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
 
-Module for tasks in the to-do list
+Module handles tasks in the to-do list
 
 """
 
 
 class TodoTask:
     """
-    Represents a task in the to-do list
+    Class represents a task in the to-do list
     """
 
     def __init__(self, description, position = None, completionStatus = 'todo'):
@@ -19,8 +19,6 @@ class TodoTask:
         self.description = description
         self.position = position
         self.completionStatus = completionStatus
-        self.visible = True
-        self.mode = 0
 
     def __str__(self):
         """
